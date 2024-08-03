99a6a3a78a08e4553b0c4ed958c00bead993cede
1omsingh <114354808+1omsingh@users.noreply.github.com>
2024-08-03 22:51:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26426018"))
 API_HASH = getenv("API_HASH", "db3addaeb6a7d9a1c8c4d5be80f68d8f")
-BOT_TOKEN = getenv("BOT_TOKEN", "7268291181:AAHoTaFWN0Ra-S879X0w5Bof3cK4EmTf4oM")
+BOT_TOKEN = getenv("BOT_TOKEN", "7255526540:AAHr9ywX-k8QlG_fEv8n0wRLczwcKqnGYqI")
 OWNER_ID = int(getenv("OWNER_ID", "6074862505"))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://omsingh9star:7h318NC2FlN40Fbf@cluster0.bjxpq4k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://Om2323705:<password>@cluster0.bduwozx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb+srv://Om2323705:oHu6KwfZ2Re7jGmf@cluster0.bduwozx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = int(getenv("LOG_GROUP", "2178318117"))
 FORCESUB = getenv("FORCESUB", "hdhhdyu_om")
